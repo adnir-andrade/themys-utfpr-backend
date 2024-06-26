8634e58fe7743039b2b48d10bb5a5ad5aab65389
Adnir Andrade <adnir_andrade@hotmail.com>
2024-06-25 22:48:11 -0300

    Add methods to seedling process

--- database/Populate/populate.php
+++ database/Populate/populate.php
@@ -3,10 +3,14 @@
 require __DIR__ . '/../../config/bootstrap.php';
 
 use Core\Database\Database;
-use Database\Populate\ProblemsPopulate;
+use Database\Populate\CampaignsPopulate;
 use Database\Populate\UsersPopulate;
+use Database\Populate\CampaignsPlayersPopulate;
+use Database\Populate\CharactersPopulate;
 
 Database::migrate();
 
-UsersPopulate::populate();
-ProblemsPopulate::populate();
+UsersPopulate::populate(12, 3);
+CampaignsPopulate::populate(3);
+CampaignsPlayersPopulate::populate(4);
+CharactersPopulate::populate();
