d582c83e6ac0cdb060d0bd8d07dea08d33036bc0
Adnir Andrade <adnir_andrade@hotmail.com>
2024-06-25 22:47:46 -0300

    Add schema for models

--- database/schema.sql
+++ database/schema.sql
@@ -2,28 +2,73 @@ SET foreign_key_checks = 0;
 
 DROP TABLE IF EXISTS users;
 
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(50) UNIQUE NOT NULL,
-    encrypted_password VARCHAR(255) NOT NULL,
-    avatar_name VARCHAR(65)
+CREATE TABLE users
+(
+  id                 int AUTO_INCREMENT PRIMARY KEY,
+  name               varchar(255)        NOT NULL,
+  username           varchar(255) UNIQUE NOT NULL,
+  email              varchar(255)        NOT NULL,
+  encrypted_password VARCHAR(255)        NOT NULL,
+  role               varchar(255)        NOT NULL
 );
 
-DROP TABLE IF EXISTS problems;
+DROP TABLE IF EXISTS campaigns;
 
-CREATE TABLE problems (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE
+CREATE TABLE campaigns
+(
+  id           int AUTO_INCREMENT PRIMARY KEY,
+  dm_id        int          NOT NULL,
+  name         varchar(255) NOT NULL,
+  next_session date,
+  FOREIGN KEY (dm_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
-DROP TABLE IF EXISTS problem_user_reinforce;
+DROP TABLE IF EXISTS campaigns_players;
 
-CREATE TABLE problem_user_reinforce (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL REFERENCES users(id) ON DELETE RESTRICT,
-    problem_id INT NOT NULL REFERENCES users(id) ON DELETE RESTRICT
+CREATE TABLE campaigns_players
+(
+  id           int AUTO_INCREMENT PRIMARY KEY,
+  player_id    int NOT NULL,
+  campaign_id  int NOT NULL,
+  character_id int,
+  FOREIGN KEY (player_id) REFERENCES users (id) ON DELETE CASCADE,
+  FOREIGN KEY (campaign_id) REFERENCES campaigns (id) ON DELETE CASCADE,
+  FOREIGN KEY (character_id) REFERENCES characters (id) ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS characters;
+
+CREATE TABLE characters
+(
+  id           int AUTO_INCREMENT PRIMARY KEY,
+  player_id    int          NOT NULL,
+  name         varchar(255) NOT NULL,
+  level        int          NOT NULL DEFAULT 1,
+  gender       varchar(255),
+  race         varchar(255) NOT NULL,
+  klass        varchar(255) NOT NULL,
+  klass_level  int          NOT NULL DEFAULT 1,
+  hp           int          NOT NULL,
+  strength     int          NOT NULL DEFAULT 10,
+  dexterity    int          NOT NULL DEFAULT 10,
+  constitution int          NOT NULL DEFAULT 10,
+  intelligence int          NOT NULL DEFAULT 10,
+  wisdom       int          NOT NULL DEFAULT 10,
+  charisma     int          NOT NULL DEFAULT 10,
+  background   varchar(255),
+  FOREIGN KEY (player_id) REFERENCES users (id) ON DELETE CASCADE
+
+);
+
+DROP TABLE IF EXISTS profile_images;
+
+CREATE TABLE profile_images
+(
+  id        int AUTO_INCREMENT PRIMARY KEY,
+  url       varchar(255) NOT NULL DEFAULT './assets/image/avatar.jpg',
+  entity_id int          NOT NULL,
+  context   varchar(10)  NOT NULL,
+  constraint unique (entity_id, context)
 );
 
 SET foreign_key_checks = 1;
