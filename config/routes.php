1ada492e2dedddf1681d8c924283f03bd59a2800
Adnir Andrade <adnir_andrade@hotmail.com>
2024-06-26 21:50:20 -0300

    Remove unused routes

--- config/routes.php
+++ config/routes.php
@@ -1,10 +1,8 @@
 <?php
 
 use App\Controllers\AuthenticationsController;
-use App\Controllers\ProblemsController;
 use App\Controllers\UsersController;
 use App\Controllers\ProfileController;
-use App\Controllers\ReinforceProblemsController;
 use Core\Router\Route;
 
 // Authentication
@@ -12,24 +10,6 @@ Route::get('/login', [AuthenticationsController::class, 'new'])->name('users.log
 Route::post('/login', [AuthenticationsController::class, 'authenticate'])->name('users.authenticate');
 
 Route::middleware('auth')->group(function () {
-  Route::get('/', [ProblemsController::class, 'index'])->name('root');
-
-  // Create
-  Route::get('/problems/new', [ProblemsController::class, 'new'])->name('problems.new');
-  Route::post('/problems', [ProblemsController::class, 'create'])->name('problems.create');
-
-  // Retrieve
-  Route::get('/problems', [ProblemsController::class, 'index'])->name('problems.index');
-  Route::get('/problems/page/{page}', [ProblemsController::class, 'index'])->name('problems.paginate');
-  Route::get('/problems/{id}', [ProblemsController::class, 'show'])->name('problems.show');
-
-  // Update
-  Route::get('/problems/{id}/edit', [ProblemsController::class, 'edit'])->name('problems.edit');
-  Route::put('/problems/{id}', [ProblemsController::class, 'update'])->name('problems.update');
-
-  // Delete
-  Route::delete('/problems/{id}', [ProblemsController::class, 'destroy'])->name('problems.destroy');
-
   // Logout
   Route::get('/logout', [AuthenticationsController::class, 'destroy'])->name('users.logout');
 
@@ -43,21 +23,4 @@ Route::middleware('auth')->group(function () {
   Route::post('/users', [UsersController::class, 'create'])->name('users.create');
   Route::put('/users/{id}', [UsersController::class, 'update'])->name('users.update');
   Route::delete('/users/{id}', [UsersController::class, 'destroy'])->name('users.destroy');
-
-
-  // Reinforce Problems
-  Route::get('/reinforce/problems', [ReinforceProblemsController::class, 'index'])
-    ->name('reinforce.problems');
-  Route::get('/reinforce/problems/page/{page}', [ReinforceProblemsController::class, 'index'])
-    ->name('reinforce.problems.paginate');
-
-  Route::get('/reinforce/problems/supported', [ReinforceProblemsController::class, 'supported'])
-    ->name('reinforce.problems.supported');
-
-  Route::post('/reinforce/problems/{id}', [ReinforceProblemsController::class, 'support'])
-    ->name('reinforce.problems.create');
-  Route::post(
-    '/reinforce/problems/{id}/stopped-supporting',
-    [ReinforceProblemsController::class, 'stoppedSupporting']
-  )->name('reinforce.problems.stopped-supporting');
 });
