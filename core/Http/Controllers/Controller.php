b5003451bbeb66ad11c47cb3be18f0ab3fcedee6
Adnir Andrade <adnir_andrade@hotmail.com>
2024-06-26 21:57:13 -0300

    Refactor indentation and add header to accept content-type as JSON to all requests

--- core/Http/Controllers/Controller.php
+++ core/Http/Controllers/Controller.php
@@ -8,61 +8,62 @@ use Lib\Authentication\Auth;
 
 class Controller
 {
-    protected string $layout = 'application';
+  protected string $layout = 'application';
 
-    protected ?User $current_user = null;
+  protected ?User $current_user = null;
 
-    public function __construct()
-    {
-        $this->current_user = Auth::user();
+  public function __construct()
+  {
+    header('Content-Type: application/json; chartset=utf-8');
+    $this->current_user = Auth::user();
+  }
+
+  public function currentUser(): ?User
+  {
+    if ($this->current_user === null) {
+      $this->current_user = Auth::user();
     }
 
-    public function currentUser(): ?User
-    {
-        if ($this->current_user === null) {
-            $this->current_user = Auth::user();
-        }
+    return $this->current_user;
+  }
 
-        return $this->current_user;
-    }
+  /**
+   * @param array<string, mixed> $data
+   */
+  protected function render(string $view, array $data = []): void
+  {
+    extract($data);
 
-    /**
-     * @param array<string, mixed> $data
-     */
-    protected function render(string $view, array $data = []): void
-    {
-        extract($data);
-
-        $view = Constants::rootPath()->join('app/views/' . $view . '.phtml');
-        require Constants::rootPath()->join('app/views/layouts/' . $this->layout . '.phtml');
-    }
+    $view = Constants::rootPath()->join('app/views/' . $view . '.phtml');
+    require Constants::rootPath()->join('app/views/layouts/' . $this->layout . '.phtml');
+  }
 
 
-    /**
-     * @param array<string, mixed> $data
-     */
-    protected function renderJson(string $view, array $data = []): void
-    {
-        extract($data);
+  /**
+   * @param array<string, mixed> $data
+   */
+  protected function renderJson(string $view, array $data = []): void
+  {
+    extract($data);
 
-        $view = Constants::rootPath()->join('app/views/' . $view . '.json.php');
-        $json = [];
+    $view = Constants::rootPath()->join('app/views/' . $view . '.json.php');
+    $json = [];
 
-        header('Content-Type: application/json; chartset=utf-8');
-        require $view;
-        echo json_encode($json);
-        return;
-    }
+    header('Content-Type: application/json; chartset=utf-8');
+    require $view;
+    echo json_encode($json);
+    return;
+  }
 
-    protected function redirectTo(string $location): void
-    {
-        header('Location: ' . $location);
-        exit;
-    }
+  protected function redirectTo(string $location): void
+  {
+    header('Location: ' . $location);
+    exit;
+  }
 
-    protected function redirectBack(): void
-    {
-        $referer = $_SERVER['HTTP_REFERER'] ?? '/';
-        $this->redirectTo($referer);
-    }
+  protected function redirectBack(): void
+  {
+    $referer = $_SERVER['HTTP_REFERER'] ?? '/';
+    $this->redirectTo($referer);
+  }
 }
