54b2888776a249822477714412d07f4eb1da476b
Adnir Andrade <adnir_andrade@hotmail.com>
2024-06-26 21:51:11 -0300

    Refactor indentation and implement "getBody" to get JSON

--- core/Http/Request.php
+++ core/Http/Request.php
@@ -4,58 +4,63 @@ namespace Core\Http;
 
 class Request
 {
-    private string $method;
-    private string $uri;
+  private string $method;
+  private string $uri;
 
-    /** @var mixed[] */
-    private array $params;
+  /** @var mixed[] */
+  private array $params;
 
-    /** @var array<string, string> */
-    private array $headers;
+  /** @var array<string, string> */
+  private array $headers;
 
-    public function __construct()
-    {
-        $this->method = $_REQUEST['_method'] ?? $_SERVER['REQUEST_METHOD'];
-        $this->uri = $_SERVER['REQUEST_URI'];
-        $this->params = $_REQUEST;
-        $this->headers = function_exists('getallheaders') ? getallheaders() : [];
-    }
+  public function __construct()
+  {
+    $this->method = $_REQUEST['_method'] ?? $_SERVER['REQUEST_METHOD'];
+    $this->uri = $_SERVER['REQUEST_URI'];
+    $this->params = $_REQUEST;
+    $this->headers = function_exists('getallheaders') ? getallheaders() : [];
+  }
 
-    public function getMethod(): string
-    {
-        return $this->method;
-    }
+  public function getMethod(): string
+  {
+    return $this->method;
+  }
 
-    public function getUri(): string
-    {
-        return $this->uri;
-    }
+  public function getUri(): string
+  {
+    return $this->uri;
+  }
 
-    /** @return mixed[] */
-    public function getParams(): array
-    {
-        return $this->params;
-    }
+  /** @return mixed[] */
+  public function getParams(): array
+  {
+    return $this->params;
+  }
 
-    /** @return array<string, string>*/
-    public function getHeaders(): array
-    {
-        return $this->headers;
-    }
+  /** @return array<string, string> */
+  public function getHeaders(): array
+  {
+    return $this->headers;
+  }
 
-    /** @param mixed[] $params*/
-    public function addParams(array $params): void
-    {
-        $this->params = array_merge($this->params, $params);
-    }
+  /** @param mixed[] $params */
+  public function addParams(array $params): void
+  {
+    $this->params = array_merge($this->params, $params);
+  }
 
-    public function acceptJson(): bool
-    {
-        return (isset($_SERVER['HTTP_ACCEPT']) && $_SERVER['HTTP_ACCEPT'] === 'application/json');
-    }
+  public function acceptJson(): bool
+  {
+    return (isset($_SERVER['HTTP_ACCEPT']) && $_SERVER['HTTP_ACCEPT'] === 'application/json');
+  }
 
-    public function getParam(string $key, mixed $default = null): mixed
-    {
-        return $this->params[$key] ?? $default;
-    }
+  public function getParam(string $key, mixed $default = null): mixed
+  {
+    return $this->params[$key] ?? $default;
+  }
+
+  public function getBody(): string
+  {
+    return file_get_contents('php://input');
+  }
 }
